686e1f39595dd47d59f126b8ae8b6af6c9f50aed
instant-bqml <instantbqml@gmail.com>
2024-09-23 12:16:12 -0400

    Determine if task succeeded or failed based on response

--- backend/controller/result/views.py
+++ backend/controller/result/views.py
@@ -57,12 +57,16 @@ class ResultResource(Resource):
         pipeline_id=job.pipeline_id,
         job_id=job.id
       )
-      job.task_failed(res.task_name)
-      # Acknowledge receipt without further processing
+      if res.success:
+        for worker_enqueue_args in res.workers_to_enqueue:
+          job.enqueue(*worker_enqueue_args)
+        job.task_succeeded(res.task_name)
+      else:
+        job.task_failed(res.task_name)
       return 'Task already processed or unknown.', 200
     if res.success:
       for worker_enqueue_args in res.workers_to_enqueue:
-          job.enqueue(*worker_enqueue_args)
+        job.enqueue(*worker_enqueue_args)
       job.task_succeeded(res.task_name)
     else:
       job.task_failed(res.task_name)
